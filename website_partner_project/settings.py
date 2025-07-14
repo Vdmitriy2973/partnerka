60df0f8b67b0ee8042e8364ec38c5e1c5a422a9e
vinokurov.d <vinokurovdmitriy2973@gmail.com>
2025-07-14 11:37:55 +0300

    Изменения в settings.py

--- website_partner_project/settings.py
+++ website_partner_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG",'False') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 LANGUAGE_CODE = 'ru'
