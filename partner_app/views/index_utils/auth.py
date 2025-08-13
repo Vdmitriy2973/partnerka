1fe627cf9dec2766c5d64fb8e7e03bded344145e
vinokurov.d <vinokurovdmitriy2973@gmail.com>
2025-08-13 16:56:30 +0300

    Небольшое исправление

--- partner_app/views/index_utils/auth.py
+++ partner_app/views/index_utils/auth.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect
 from django.contrib.auth import login
 from django.contrib import messages
-from partner_app.utils import send_email_via_mailru
 from partner_app.forms import LoginForm
 
 def handle_login(request):
@@ -13,7 +12,6 @@ def handle_login(request):
             print("remembered")
             request.session.set_expiry(1209600)
         login(request, user)
-        send_email_via_mailru(user.email,"Произошла авторизация с нового устройства","🔒 Обнаружен вход в ваш аккаунт с нового устройства")
         return redirect("dashboard")
     else:
         messages.error(request, "Неверный email или пароль.",extra_tags="login_error")
